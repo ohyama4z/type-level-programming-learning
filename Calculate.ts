db9568f4a3fead151a9fe4764662b4a7959b8405
ohyama4z <45777685+ohyama4z@users.noreply.github.com>
2021-09-21 08:42:24 +0900

    剰余を再帰数が少なくなるように修正

--- Calculate.ts
+++ Calculate.ts
@@ -1,13 +1,13 @@
-import { Repeat, ToNumber } from "./TupleToLiteral"
+import { Repeat } from "./TupleToLiteral"
 
-type Add<A extends number, B extends number> = [
+export type Add<A extends number, B extends number> = [
   ...Repeat<any, A>,
   ...Repeat<any, B>
 ][`length`]
 
 type add1 = Add<10, 3> // 13
 
-type Sub<A extends number, B extends number> = Repeat<any, A> extends [
+export type Sub<A extends number, B extends number> = Repeat<any, A> extends [
   ...Repeat<any, B>,
   ...infer R
 ]
@@ -16,7 +16,7 @@ type Sub<A extends number, B extends number> = Repeat<any, A> extends [
 
 type sub1 = Sub<10, 3> // 7
 
-type Mul<A extends number, B extends number> = A extends 0
+export type Mul<A extends number, B extends number> = A extends 0
   ? 0
   : B extends 0
   ? 0
@@ -26,7 +26,7 @@ type Mul<A extends number, B extends number> = A extends 0
 
 type mul1 = Mul<20, 3> // 60
 
-type Div<A extends number, B extends number> = A extends 0
+export type Div<A extends number, B extends number> = A extends 0
   ? 0
   : B extends 0
   ? never
@@ -36,8 +36,8 @@ type Div<A extends number, B extends number> = A extends 0
 
 type div1 = Div<12, 3> // 4
 
-type Mod<A extends number, B extends number> = Sub<A, B> extends never
+export type Mod<A extends number, B extends number> = Sub<A, B> extends never
   ? A
-  : Sub<A, Mul<B, Div<A, B>>>
+  : Mod<Sub<A, B>, B>
 
 type mod1 = Mod<11, 3> // 2
